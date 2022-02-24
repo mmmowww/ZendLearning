776657ccd0d5644611fccc51f0359545ec3844ae
Selistral <NeksusVita404@mail.ru>
2022-02-24 21:12:31 +0300

    Add seters and geter manual not command :(

--- module/Album/src/Album/Entity/Foto.php
+++ module/Album/src/Album/Entity/Foto.php
@@ -10,23 +10,37 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Table(name="foto")
  * @ORM\Entity
  */
-class Foto
-{
-    /**
-     * @var int
-     *
-     * @ORM\Column(name="Id", type="integer", nullable=false)
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="IDENTITY")
-     */
-    private $id;
+class Foto {
+	/**
+	 * @var int
+	 *
+	 * @ORM\Column(name="Id", type="integer", nullable=false)
+	 * @ORM\Id
+	 * @ORM\GeneratedValue(strategy="IDENTITY")
+	 */
+	private $id;
 
-    /**
-     * @var string|null
-     *
-     * @ORM\Column(name="src", type="text", length=65535, nullable=true)
-     */
-    private $src;
-
+	/**
+	 * @var string|null
+	 *
+	 * @ORM\Column(name="src", type="text", length=65535, nullable=true)
+	 */
+	private $src;
+	/**
+	 * Get id
+	 *
+	 * @return string
+	 */
+	public function getId() {
+		return $this->id;
+	}
+	/**
+	 * Get id
+	 *
+	 * @return string
+	 */
+	public function getSrc() {
+		return $this->src;
+	}
 
 }
